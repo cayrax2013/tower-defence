1d62665251e53d139d045ed778634984e304efbc
cayrax2013 <54445011+cayrax2013@users.noreply.github.com>
2020-04-22 16:24:34 +0300

    Added my new game Tower Defence

---
+++ Assets/Scripts/TowerShooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private float _delayBetweenShots = 2f;
+
+    private float _elapsedTime = 0f;
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _delayBetweenShots)
+        {
+            _elapsedTime = 0f;
+            Instantiate(_bullet);
+        }
+    }
+}
